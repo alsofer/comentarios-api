a17ed78888e5a881d587d83bbedff5a1a7523e62
alsofer <engenharia@alsofer.com>
2021-08-06 16:58:05 -0300

    retirando models, schemas e crud para refazer modelagem com o bd

--- api/main.py
+++ api/main.py
@@ -4,11 +4,8 @@ from pydantic import BaseModel
 from mangum import Mangum
 from typing import List
 from sqlalchemy.orm import Session
-from . import crud, models, schemas
-from .database import SessionLocal, engine
 
 
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='Comentários')
 
@@ -20,24 +17,16 @@ app.add_middleware(
     allow_headers=["x-apigateway-header", "Content-Type", "X-Amz-Date"],
 )
 
-def get_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-
 @app.get('/healthcheck', name='HealthCheck da aplicação', tags=['HealthCheck'])
 def healthcheck():
     return {"OK!"}
 
-@app.get('/comment/list', response_model=schemas.Comment, name='Listar comentários', tags=['List'])
-def list_comments(db: Session = Depends(get_db)):
-    comment = db.query(models.Comment).all()
-    return comment
+@app.get('/comment/list', name='Listar comentários', tags=['List'])
+def list_comments():
+    return {"Sample"}
 
-@app.post('/comment/new', response_model=schemas.Comment, name='Adicionar um novo comentário', tags=['Add'])
-def new_comment(comment: schemas.Comment, db: Session = Depends(get_db)):
-    return crud.new_comment(db=db, comment=comment)
+@app.post('/comment/new', name='Adicionar um novo comentário', tags=['Add'])
+def new_comment():
+    return {"Sample"}
 
 handler = Mangum(app, debug=True, enable_lifespan=False, spec_version=3)
