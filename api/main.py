c58d6eb2535f08034b4b1834f3c28f8460f184c6
alsofer <engenharia@alsofer.com>
2021-08-07 16:45:20 -0300

    corrigindo

--- api/main.py
+++ api/main.py
@@ -26,7 +26,7 @@ def get_db() -> Generator:
 comments= json.load(open("comments.json", "r"))
 app = FastAPI(
     title="Comentários API",
-    version=0.1
+    version=0.1,
     root_path="api"
     )
 
