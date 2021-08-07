99eaef18a300460bef7b2182dfa4eaf0ec6ac413
alsofer <engenharia@alsofer.com>
2021-08-07 03:14:17 -0300

    tentando outra bordagem para modelagem de dados

--- api/main.py
+++ api/main.py
@@ -1,18 +1,19 @@
 from starlette.middleware.cors import CORSMiddleware
-from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi.openapi.utils import get_openapi
 from pydantic import BaseModel
 from mangum import Mangum
-from typing import List
+from typing import List, Dict, Optional, Union
 from sqlalchemy.orm import Session
+from datetime import datetime
+import json
 import os
 
-
+comments= json.load(open("comments.json", "r"))
 
 app = FastAPI(
     title="Comentários",
-    version=0.1,
-    root_path="api",
-    openapi_prefix=os.getenv('ROOT_PATH', '')
+    version=0.1
     )
 
 app.add_middleware(
@@ -23,20 +24,47 @@ app.add_middleware(
     allow_headers=["x-apigateway-header", "Content-Type", "X-Amz-Date"],
 )
 
-@app.get("/")
-def read_root(request: Request):
-    return {"message": "Hello World", "root_path": request.scope.get("root_path")}
 
-@app.get('/healthcheck', name='HealthCheck da aplicação', tags=['HealthCheck'])
-def healthcheck():
-    return {"OK!"}
+@app.get("/health/")
+def health() -> Dict[str, datetime]:
+    return {"timestamp": datetime.now()}
 
-@app.get('/comment/list', name='Listar comentários', tags=['List'])
-def list_comments():
-    return {"Sample"}
+@app.get("/comments/")
+def get_all_comments() -> List[Dict[str, Union[float, int, str]]]:
+    if response := comments:
+        return response
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Não existem comentários sendo feitos.",
+    )
 
-@app.post('/comment/new', name='Adicionar um novo comentário', tags=['Add'])
-def new_comment():
-    return {"Sample"}
+@app.get("/comments/{content_id}/")
+def get_comments(content_id: int) -> Dict[str, Union[float, int, str]]:
+    if response := list(
+        filter(lambda i: i.get("content_id") == content_id, comments)
+    )[0]:
+        return response
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Não foram encontrados comentários sobre o conteúdo '{content_id}' ",
+    )
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Comentários API",
+        version="0.1",
+        description="Api de comentários online",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://upload.wikimedia.org/wikipedia/pt/2/22/Logotipo_da_Rede_Globo.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 handler = Mangum(app, debug=True, enable_lifespan=False, spec_version=3)
